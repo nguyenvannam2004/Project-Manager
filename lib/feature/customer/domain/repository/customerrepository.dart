f7c6e01a4f5b11d5ec49893bf6b4c1c9913d8a04
NamNguyen <nguyenvannam2004.hd@gmail.com>
2025-10-27 16:47:04 +0700

    Thu nghiem ket noi voi backend asp.aspnet

--- lib/feature/customer/domain/repository/customerrepository.dart
+++ lib/feature/customer/domain/repository/customerrepository.dart
@@ -6,5 +6,5 @@ abstract class CustomerRepository {
   Future<List<Customer>> getAllCustomer();
   Future<void> creatCustomer(Customer customer);
   Future<void> updateCustomer(Customer customer);
-  Future<void> deleteCustomer(String id);
+  Future<void> deleteCustomer(int id);
 }
